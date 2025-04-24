1c1a8bca24f403fcfce804ebd1906550cf06bfd3
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2025-04-24 14:22:40 +0530

    Update resetPassword.js

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async (req, res) => {
 
 
         // create url
-        const url = `http://localhost:5173/update-password/${token}`;
+        const url = `https://study-notion-frontend-b2o4-git-main-ramlakhan-guptas-projects.vercel.app/update-password/${token}`; // update email route
 
         // send email containing url
         await mailSender(email, 'Password Reset Link', `Password Reset Link : ${url}`);
